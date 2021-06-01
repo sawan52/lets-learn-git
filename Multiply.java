c776beb50373c670d3103904d705c73f4e753e51
Sawan Singh <sawansingh5216977@gmail.com>
2021-06-02 00:11:30 +0530

    added multiply feature

---
+++ Multiply.java
@@ -0,0 +1,8 @@
+public class Sum{
+    public static void main(String[] args) {
+        
+        int a = 5;
+        int b = 10;
+        System.out.println("Multiplication of " + a + " and " + b + " is " + (a * b));
+    }
+}
